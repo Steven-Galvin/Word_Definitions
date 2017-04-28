2cac5b1fcb5ee32e6dee97cb4aa6b73bacc653b0
Steven Galvin <steven.john.galvin@gmail.com>
2017-04-28 14:18:31 -0700

    test and pass second integration spec. enter word and definition, hit add word, app generates entered word and hyper link to take user to next page containing definition.

--- app.rb
+++ app.rb
@@ -14,6 +14,13 @@ end
 post('/words') do
   word = params.fetch('word')
   @word = Words.new({:word => word})
+  definition = params.fetch('definition')
+  @word.add_definition(definition)
   @word.save()
   redirect('/')
+end
+
+get('/word/:id') do
+  @word = Words.find(params.fetch("id"))
+  erb(:word)
 end
--- spec/Word_integration_spec.rb
+++ spec/Word_integration_spec.rb
@@ -5,11 +5,24 @@ set(:show_exceptions, false)
 
 describe("the word/definitions path", {:type => :feature}) do
 
+  before do
+    Words.clear()
+  end
+
   it('creates word from index page') do
     visit('/')
     fill_in('word', :with => 'test_word')
     click_button('Add Word!')
     expect(page).to have_content('test_word')
+  end
+
+  it('allows user to click on returned words and generate new page corresponding with word showing first definition') do
+    visit('/')
+    fill_in('word', :with => 'test_word')
+    fill_in('definition', :with => 'test_definition')
+    click_button('Add Word!')
+    click_link('test_word')
+    expect(page).to have_content('test_definition')
   end
 
 end
